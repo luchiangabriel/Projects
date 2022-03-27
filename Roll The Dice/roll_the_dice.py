3a81c55226aa987e8ff573f241106a843c1dbc88
Luchian Gabriel <igabe98@icloud.com>
2022-03-27 23:15:33 +0300

    Update roll_the_dice.py

--- Roll The Dice/roll_the_dice.py
+++ Roll The Dice/roll_the_dice.py
@@ -1,19 +1,25 @@
 
 import random
+import time
 
 minim = 1
 maxim = 6
 
-print("""
-Welcome to the Roll The Dice Game!
-    Do you want to play it?
-      [y (Yes) / n (no)]\n""")
-roll = input("Choice: ")
+print("Welcome to the Roll The Dice Game!")
 
+roll = True
 
-while roll == "y":
-    print("\nRolling the dices...")
-    print("The values are:", random.randint(minim, maxim), ",", random.randint(minim, maxim))
-    roll = input("\nRoll the dices again? [y (Yes) / n (no)]\nChoice: ").lower()
-
-print("\nGood sesion of gaming!")
+while roll:
+    print("""
+  Do you want to play it?
+    [y (Yes) / n (no)]\n""")
+    roll = input("Choice: ").lower()
+    if roll == 'y':
+        print("\nRolling the dices...")
+        time.sleep(1)
+        print("The values are:", random.randint(minim, maxim), ",", random.randint(minim, maxim))
+    elif roll == 'n':
+        print("\nThank you for playing the game!")
+        roll = False
+    else:
+        print("Invalid input")
