b125228866ab30cbbbd4c35aebc9e64aab1a0cf3
Luchian Gabriel <igabe98@icloud.com>
2022-03-28 00:05:33 +0300

    Update number_guessing.py

--- Number Guessing/number_guessing.py
+++ Number Guessing/number_guessing.py
@@ -12,20 +12,9 @@ x = computer()
 
 guess = 1
 
-
-def hint():
-    if x - 10 <= guess < x - 5 or x + 5 < guess <= x + 10:
-        print("Warm!")
-    elif x - 5 <= guess < x or x < guess <= x + 5:
-        print("Hot!")
-    elif x == guess:
-        pass
-    else:
-        print("Cold!")
-
-
 tries = 7
 while guess != x:
+
     if guess in range(1, 100):
         print("\nTry to guess the number!")
         guess = int(input("Number: "))
@@ -35,11 +24,24 @@ while guess != x:
         guess = int(input("Number: "))
 
     if tries == 0:
-        print("\nOut of tries.\nMaybe next time!")
+        print("\nOut of tries!\nAI Wins!\nMaybe next time!")
         break
+
+    def hint():
+        if x - 10 <= guess < x - 5 or x + 5 < guess <= x + 10:
+            print("Warm!")
+        elif x - 5 <= guess < x or x < guess <= x + 5:
+            print("Hot!")
+        elif x == guess:
+            pass
+        else:
+            print("Cold!")
+
     hint()
     tries -= 1
+
 else:
+
     print("""
 |-----------|
 | Found it! |
